a7dc657764fcbfc0d57212afde0b761dd1c54bea
Nalo-DO-IT <davidbgh91@gmail.com>
2023-04-06 16:07:02 +0200

    Add resource config and FileReader

--- ImaTxt/src/main/java/fileReader/lecturaFicheros.java
+++ ImaTxt/src/main/java/fileReader/lecturaFicheros.java
@@ -1,18 +1,17 @@
 package fileReader;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.io.File;
 
-public class fileReader {
+public class lecturaFicheros {
 
-    private List<String> readLinesFiles(Path filePath) throws IOException{
+    public List<String> readLinesFiles(Path filePath) throws IOException{
         List<String> lines = new ArrayList<>();
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath.toFile()))){
+        try (BufferedReader br = new BufferedReader(new java.io.FileReader(filePath.toFile()))){
             String line;
             while ((line = br.readLine()) !=null ) {
                 lines.add(line);
