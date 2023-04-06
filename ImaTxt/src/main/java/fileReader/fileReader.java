480e51210b01e121721016adc22ada91501b5e11
AudeTercero <ikerdam1b@gmail.com>
2023-04-06 14:40:14 +0200

    Add resource config and FileReader

---
+++ ImaTxt/src/main/java/fileReader/fileReader.java
@@ -0,0 +1,39 @@
+package fileReader;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
+
+public class fileReader {
+
+    private List<String> readLinesFiles(Path filePath) throws IOException{
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath.toFile()))){
+            String line;
+            while ((line = br.readLine()) !=null ) {
+                lines.add(line);
+            }
+
+        }
+        return lines;
+    }
+
+    private List<String> listTextFiles(Path filePath) {
+        List<String> textFiles = new ArrayList<>();
+        File folder = new File(filePath.toString());
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            if (file.isFile() && file.getName().endsWith(".txt")) {
+                textFiles.add(file.getName());
+            }
+        }
+        return textFiles;
+    }
+
+
+
+}
