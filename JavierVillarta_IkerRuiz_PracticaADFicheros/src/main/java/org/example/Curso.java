ed14aa0c790d8c33110ecf4f02738e130cb6b219
Javilmar <errinostrich@gmail.com>
2023-11-05 18:14:31 +0100

    terminado cursos

--- JavierVillarta_IkerRuiz_PracticaADFicheros/src/main/java/org/example/Curso.java
+++ JavierVillarta_IkerRuiz_PracticaADFicheros/src/main/java/org/example/Curso.java
@@ -1,32 +1,81 @@
 package org.example;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class Curso {
     private int codCur;
     private String nombre;
     private String descripcion;
-    private Profesor profe;
-    private HashMap<Integer, Alumno> alumnos;
-    private static int cont = 0;
+    private String profe;
+    private String[] alumnos;
+    private static final String FICHERO = "Cursos.txt";
 
     /**
      * @param nombre
      * @param descripcion
      */
     public Curso(String nombre, String descripcion) {
-        this.codCur = cont;
+        this.codCur = nuevoIdentificador();
         this.nombre = nombre;
         this.descripcion = descripcion;
-        cont++;
+
     }
 
-    public Curso(int codCur, String nombre, String descripcion, Profesor profe, HashMap<Integer, Alumno> alumnos) {
-        this.codCur = codCur;
+    public Curso(int id, String nombre, String descripcion, String profe, String[] alumnos) {
+        this.codCur = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.profe = profe;
         this.alumnos = alumnos;
+    }
+
+    public int nuevoIdentificador() {
+        int aux = 0;
+        ArrayList<Curso> cursos = leerFich();
+        if (!cursos.isEmpty()) {
+            for (Curso c : cursos) {
+                if (c.getCodCur() > aux) {
+                    aux = c.getCodCur();
+                }
+            }
+            aux++;
+        } else {
+            aux = 1;
+        }
+        return aux;
+
+    }
+
+    public ArrayList<Curso> leerFich() {
+        File file = new File(FICHERO);
+        ArrayList<Curso> cursos = new ArrayList<>();
+        String cod, nom, des;
+        String prof;
+        String[] alumnos;
+
+        if (file.exists()) {
+            try (BufferedReader br = new BufferedReader(new FileReader(FICHERO))) {
+                String line;
+                while ((cod = br.readLine()) != null) {
+                    cod = cod.split(":")[1].trim();
+                    int id = Integer.parseInt(cod);
+                    nom = br.readLine().split(":")[1].trim();
+                    des = br.readLine().split(":")[1].trim();
+                    prof = br.readLine().split(":")[1].trim();
+                    alumnos = br.readLine().split(":")[1].trim().split(",");
+
+                    Curso c = new Curso(id, nom, des, prof, alumnos);
+                    cursos.add(c);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return cursos;
     }
 
     public int getCodCur() {
@@ -53,12 +102,20 @@ public class Curso {
         this.descripcion = descripcion;
     }
 
-    public HashMap<Integer, Alumno> getCursos() {
+    public String getProfe() {
+        return profe;
+    }
+
+    public void setProfe(String profe) {
+        this.profe = profe;
+    }
+
+    public String[] getAlumnos() {
         return alumnos;
     }
 
-    public void setCursos(HashMap<Integer, Alumno> cursos) {
-        this.alumnos = cursos;
+    public void setAlumnos(String[] alumnos) {
+        this.alumnos = alumnos;
     }
 
     @Override
